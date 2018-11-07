58eabadfcff8cd435bffb963b7ec20e1eeb393f9
Rommel Rico <me@rommelrico.com>
2018-11-06 19:53:44 -0800

    Using a Handler and a Runnable.

--- app/src/main/java/com/rommelrico/timerdemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/timerdemo/MainActivity.kt
@@ -2,11 +2,27 @@ package com.rommelrico.timerdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Using a Handler and a Runnable.
+        val myHandler = Handler()
+        val run = object: Runnable {
+
+            override fun run() {
+                Log.i("Hey it's us!", "A second passed by")
+                myHandler.postDelayed(this, 1000)
+            }
+
+        }
+        myHandler.post(run)
+
     }
 }
